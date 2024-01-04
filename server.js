98dff8d9712b0b5c9787585e69e3892361fe72a4
Jeff <doylejeff98@gmail.com>
2024-01-04 14:03:35 -0500

    created utils and message.js with moment import

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,10 +11,12 @@ const io = socketio(server);
 // static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'ChatBoi Bot';
+
 io.on('connection', socket => {
 
     //welcomes current user
-    socket.emit('message', 'Welcome to ChatBoi');
+    socket.emit('message', formatMessage(botName, 'Welcome to ChatBoi'));
 
     // lets everyone, except for the user, aware someone connects
     socket.broadcast.emit('message', 'A wild user appeared!');
